f7eae7e863938882770e4ba7fc5ea3a6a6479663
LeahYJ <leahjb06.kodehode@gmail.com>
2024-04-02 11:38:33 +0200

    start the project

--- js/index.js
+++ js/index.js
@@ -1,36 +1,31 @@
-document.addEventListener("DOMContentLoaded", function(){
-    const btnPressed = document.querySelector("#times-pressed-btn");
-    const btnClicker = document.querySelector("#btn-clicker");
-    let btnTimesPressed = 0; 
-    let world = false;
+const welcomeMsg = "welcome too my page"
+let visitorsSite = 234
+const isSiteUp = true
+const stringLiterals = `${welcomeMsg}. number of visitors: ${visitorsSite}. site: ${isSiteUp}`
+const monsters = [ 23, 61, 277 ]
+//                  0   1   2
+monsters[1]
 
-    function countBtnPress() {
-        btnPressed.textContent = `You have pressed the button ${btnTimesPressed} times!`;
-    }
+const user1 = {
+    age: 23,
+    bestSub: "java",
+    isCertified: true,
+}
 
-    btnClicker.addEventListener("click", function() {
-        const worldTxt = document.querySelector("#world-txt");
-        world = !world;
-        btnTimesPressed++; 
-        countBtnPress();
-        if (world) {
-            worldTxt.innerText = "Goodbye World!";
-        } else {
-            worldTxt.innerText = "Hello World!";
-        }
-    });
+user1.age
+function consoleTest() {
+    console.log(stringLiterals)
+    console.log(`${monsters[1]} years old`)
+    console.log(user1.isCertified)
+    console.log("")
+    console.log("hei")
+}
 
-    const boolean = true;
-    const string = "Hei";
-    const number = 100;
-    const stringnumber = "100";
-    const array = [23, 11, 20];
-    const objects = {
-        pål: 20,
-        emma: 21,
-        per: 35,
-    };
-    const litteralString = `hei jeg har ${number} terninger`;
+console.log(24 + 2)
+console.log(24 - 2)
+console.log(24 * 2)
+console.log(24 / 2)
+console.log(25 % 2)
+console.log(welcomeMsg + " welcome back")
+console.log("2" + (2 + 2))
 
-    countBtnPress();
-});
