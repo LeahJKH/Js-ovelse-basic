896f789d73f62464333799dc5cfaae926360ef3d
LeahYJ <leahjb06.kodehode@gmail.com>
2024-04-04 13:55:25 +0200

    shown for and if

--- js/index.js
+++ js/index.js
@@ -1,31 +1,32 @@
-const welcomeMsg = "welcome too my page"
-let visitorsSite = 234
-const isSiteUp = true
-const stringLiterals = `${welcomeMsg}. number of visitors: ${visitorsSite}. site: ${isSiteUp}`
-const monsters = [ 23, 61, 277 ]
-//                  0   1   2
-monsters[1]
+const cardContain = document.getElementById("Card-Contain");
+const headingTop = document.getElementById("Heading-top");
+const cardShowBtn = document.getElementById("show-Cards");
 
-const user1 = {
-    age: 23,
-    bestSub: "java",
-    isCertified: true,
-}
+const infoP = ["hei jeg ser deg", "månen er fin", "litago er godt"];
 
-user1.age
-function consoleTest() {
-    console.log(stringLiterals)
-    console.log(`${monsters[1]} years old`)
-    console.log(user1.isCertified)
-    console.log("")
-    console.log("hei")
-}
+let cardShown = false;
 
-console.log(24 + 2)
-console.log(24 - 2)
-console.log(24 * 2)
-console.log(24 / 2)
-console.log(25 % 2)
-console.log(welcomeMsg + " welcome back")
-console.log("2" + (2 + 2))
+headingTop.innerText = "header text";
 
+makeCards();
+
+cardShowBtn.addEventListener("click", function() {
+    if(cardShown) {
+        cardShowBtn.textContent = "Show more"
+        cardContain.style.display = "none"  
+    } else {
+        cardShowBtn.textContent = "Show less"
+        cardContain.style.display = "flex"  
+    };
+    cardShown = !cardShown
+});
+
+function makeCards() {
+    for(i = 0; i < infoP.length; i++) {
+        cardContain.innerHTML += `
+        <div class="card" id="cardS${i}">
+        <p>${infoP[i]}</p>
+        </div>
+        `
+    };
+};
